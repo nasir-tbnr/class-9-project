30a63dda96a47ece5604ce03aadfd715afa6e07a
nasir-tbnr <99035902+nasir-tbnr@users.noreply.github.com>
2022-03-16 02:32:15 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+
+function setup() {
+  createCanvas(1700,800);
+  box=createSprite(850,400,20,20)
+}
+
+function draw() 
+{
+  background("yellow");
+drawSprites();
+if (keyIsDown(39)) {
+  background("red");
+  
+}
+  }
+
+
+
+
